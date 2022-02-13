f7677f1a0e507f3970b3328ce573befbb2c2142f
Hendra <hendra2392@gmail.com>
2022-02-13 16:02:01 +0700

    Update

--- sqlite3.js
+++ sqlite3.js
@@ -1,21 +1,25 @@
-var sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('app.db');
 
-db.serialize(function() {
-  db.each("SELECT id, name, age FROM students WHERE age = 55", function(_, row) {
-      console.log(row)
-  });
+db.all("SELECT id AS studentID, name, age FROM students", function(err, rows) {
+  // console.log(err)
+  console.log(rows)
 });
 
 
-let newEntry = {
-  name: 'KarenX',
-  age: 55
-}
+for(let x=20;x<30;x++){
+  let stmt = db.prepare("INSERT INTO subjects ('name') VALUES (?)", function(err){
+    if(err){
+      console.log("ERROR INSERTING DATA", err);
+    }
+    else{
+      console.log("SUCCESS INSERTING DATA", err);
+    }
+  })
 
-var stmt = db.prepare("INSERT INTO STUDENTS VALUES (NULL,?, ?)");
-stmt.run(newEntry.name, newEntry.age);
-stmt.finalize()
+  stmt.run(`subject-${x}`)
+  stmt.finalize()
+}
 
 db.close();
 
